36501f3de510c99c02193caaf2d4be88f9505af6
Moni-Yahwehson <moniyahwehson@gmail.com>
2024-01-26 14:29:32 +0100

    Working on calculating the CGPA

--- src/pages/CalculateCGPA.jsx
+++ src/pages/CalculateCGPA.jsx
@@ -1,5 +1,47 @@
+import { useState } from "react";
+
+/* eslint-disable no-unused-vars */
+const grades = {
+  A: 5,
+  B: 4,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 0,
+};
+
 function CalculateCGPA() {
-  return <div></div>;
+  const [courses, setCourses] = useState([]);
+
+  //Adding a course
+  function handleAddCourse(newCourse) {
+    setCourses([...courses, newCourse]);
+  }
+
+  //Editing a course
+  function handleEditCourse(index, updatedCourse) {
+    const updatedCourses = [...courses];
+    updatedCourses[index] = updatedCourse;
+    setCourses(updatedCourses);
+  }
+
+  //Deleting a course
+
+  function handleDeleteCourse(index) {
+    const updatedCourses = courses.filter((course, i) => i !== index);
+    setCourses(updatedCourses);
+  }
+
+  //Calculating the CGPA
+
+  const calculateGPA = function () {
+    let totalGradePoints = 0;
+    let totalCredits = 0;
+
+    courses.forEach((course) =>{
+      totalGradePoints += grades[course.grade] * course.
+    })
+  };
 }
 
 export default CalculateCGPA;
